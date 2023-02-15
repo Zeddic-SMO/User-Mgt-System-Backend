51b3889e7be2975a3529676d7567a12dbd7243af
Zeddic-SMO <samuel93ortil@gmail.com>
2023-02-15 17:36:02 +0100

    Remote DB connected

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose")
-
+const dotenv = require("dotenv")
+dotenv.config()
 mongoose.set("strictQuery", false)
 
 async function dbConnection() {
   mongoose
-    .connect("mongodb://localhost/UserMgtSystem", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nhc3uy0.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => {
       console.log("Connection to DB successful")
     })
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express")
 const app = express()
-/* const dotenv = require("dotenv")
-dotenv.config() */
 const cors = require("cors")
 
 const corsOptions = {
